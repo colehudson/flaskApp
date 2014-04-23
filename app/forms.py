78d4d85cb5df986fff6ef182653fae2a70d382af
Cole Hudson <colehudson@gmail.com>
2014-04-23 07:07:54 -0400

    need to fix virtualenv

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,19 @@
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 
 class RegistrationForm(Form):
-	username = TextField('Username', [validators.Length(min=4, max=25)])
-	email = TextField('Email Address', [validators.Length(min=6, max=35)])
-	password = PasswordField('New Password', [
-		validators.Required(),
-		validators.EqualTo('confirm', message='Passwords must match)
-	])
-	confirm = PasswordField('Repeat Password')
-	accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+
+# from flask.ext.wtf import Form
+# from wtforms import TextField, BooleanField
+# from wtforms.validators import Required
+
+# class LoginForm(Form):
+#     openid = TextField('openid', validators = [Required()])
+#     remember_me = BooleanField('remember_me', default = False)
--- app/views.py
+++ app/views.py
@@ -9,8 +9,23 @@ def index():
 def register():
 	form = Registration(request.form)
 	if request.method == 'POST' and form.validate():
-		user = User(form.useranme.data, form.email.data, form.password.data)
+		user = User(form.username.data, form.email.data, form.password.data)
 		db_session.add(user)
 		flash('Thanks for registering')
 		return redirect(url_for('login'))
 	return render_template('register.html', form=form)
+
+
+
+# from flask import render_template, flash, redirect
+# from app import app
+# from forms import LoginForm
+
+# # index view function suppressed for brevity
+
+# @app.route('/login', methods = ['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html', 
+#         title = 'Sign In',
+#         form = form)
