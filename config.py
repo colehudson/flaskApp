4a70c2f752c2a92da81f749f039901571d6628a1
Cole Hudson <cole.hudson@wayne.edu>
2014-05-02 15:16:15 -0400

    cleanup

--- config.py
+++
@@ -1,7 +0,0 @@
-from mysql_settings import mysqlConnection
-
-# note have to install mysql connector: pip install mysql-python
-SQLALCHEMY_DATABASE_URI = mysqlConnection
-
-CSRF_ENABLED = True
-SECRET_KEY = 'wow-this-is-a-difficult-key'
